f0697f865aadc099076566841418865bfe842b84
handikaadicky <36119932+handikaadicky@users.noreply.github.com>
2020-09-15 15:38:59 +0700

    get user from postgreSQL

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 const db = require('./db/models')
 
-app.get('/', (req, res) => res.send('beginning heroku'))
+app.get('/', async (req, res) => {
+    const response = await db.users.findAll()
+    res.send(response)
+});
+
+
+
 app.get('/home', (req, res) => res.send('homeheroku'))
 
 app.listen(port, (req, res) => console.log(`listen to ${port}`))
