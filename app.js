d7286920c4354ed77332f3b6030bc0a6d513f092
handikaadicky <36119932+handikaadicky@users.noreply.github.com>
2020-09-15 16:42:08 +0700

    remove sequelize db

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const db = require('./db/models')
 
-app.get('/', async (req, res) => {
-    const response = await db.users.findAll()
-    res.send(response)
+app.get('/', (req, res) => {
+    // const response = await db.users.findAll()
+    res.send('hello world')
 });
 
 
